f5458d99c57cc6951c2aa61eaa8f9bc3247bac81
Rias Van der Veken <hello@rias.be>
2017-03-24 10:13:56 +0100

    First release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All Notable changes to the `Spaceless` add-on will be documented in this file
+
+## 1.0.0 - 2017-03-24
+
+- First release
---
+++ SpacelessTags.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Statamic\Addons\Spaceless;
+
+use Statamic\Extend\Tags;
+
+class SpacelessTags extends Tags
+{
+    /**
+     * The {{ spaceless }} tag.
+     *
+     * @return string|array
+     */
+    public function index()
+    {
+        return trim(preg_replace('/>\s+</', '><', $this->content));
+    }
+
+}
